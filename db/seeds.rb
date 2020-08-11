7633f76f0b2f9399c9448cafca195b882e9d7764
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-08-11 09:50:12 -0400

    add rails g resource Project title started description:text and rails db:create and rails db:migrate and rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+projects = [{
+    title: 'Career search',
+    started: 'Today',
+    description: 'Build edit and show button by linking it to the tasks modal. These links will have to be to the nested routes',
+}, {
+    title: 'Technical Algorithms', 
+    started: 'Today',
+    description: 'Build with project requirements',
+}, {
+    title: 'Java Spring', 
+    started: 'Today',
+    description: 'Build with project requirements',
+}, {
+    title: 'Networking', 
+    started: 'Today',
+    description: 'Build with project requirements',
+}]
+
+projects.each do |project|
+    Project.create(project)
+end
