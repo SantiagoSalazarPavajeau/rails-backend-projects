183dc0c050e8500c8033f029c1702c5f079a4888
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-08-12 10:13:48 -0400

    add create action on projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,17 @@ class ProjectsController < ApplicationController
         options = {
             is_collection: true,
           }
-        render json: ProjectSerializer.new(projects).serialized_json
+        render json: ProjectSerializer.new(projects)
     end
 
+    def create
+        project = Project.create(project_params)
+        render json: ProjectSerializer.new(project).serialized_json
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(:title, :description)
+    end
 end
