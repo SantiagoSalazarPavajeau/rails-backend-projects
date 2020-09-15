a2d5159d540a9c303941e582b13ba460c091c480
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-09-15 10:29:31 -0400

    add create and delete user controller action

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,4 +4,27 @@ class PeopleController < ApplicationController
         people =  Person.all
         render json: PersonSerializer.new(people)
     end
+
+    def create
+        image = "https://api.adorable.io/avatars/#{rand(100)}"
+        new_user = Person.create(username: params[:user][:username], password: params[:user][:password], image: image)
+        if new_user
+            render json: PersonSerializer.new(new_user).serialized_json
+        end
+    end
+
+    def destroy
+        person = Person.find(people_params[:id])
+        person.delete
+        person.tasks.each do |task|
+            task.person_id = 1
+        end
+        render json: PersonSerializer.new(people)
+    end
+
+    private
+
+    def people_params
+        params.require(:project).permit(:username, :password, :image)
+    end
 end
