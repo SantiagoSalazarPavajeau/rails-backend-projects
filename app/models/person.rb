e5c8453f8ba5fd75d9251a1e3651da8cd986d91d
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-09-16 11:31:46 -0400

    add validates helper to person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ApplicationRecord
+    validates_uniqueness_of :username    
+    validates :password, presence: true
     has_many :tasks
     has_many :projects, through: :tasks
     has_secure_password
